4b8dcb739d9ae13febeb26d57ea9b219f0291859
Beka Chitadze <bchitadze@credo.ge>
2024-10-22 10:54:31 +0400

    Task_3 beqa

--- src/test/java/Methods.java
+++ src/test/java/Methods.java
@@ -2,7 +2,10 @@ public class Methods {
     public static void main(String[] args) {
             Excercise_2 Excercise_2 = new Excercise_2();
             int number = Excercise_2.findLargest();
-            System.out.println(number);
+            System.out.println("Task_2 შედეგი = " + number);
+
+            calculateArea calculateArea = new calculateArea();
+            double result = calculateArea.calculateArea();
+            System.out.println("Task_3 შედეგი = " + result);
         }
     }
-    
